ee9faceb03ada88da4eb9e32735279edf6b2b402
capp-adocia <2636427505@qq.com>
2025-10-17 23:59:16 +0800

    commit

--- Makefile
+++ Makefile
@@ -1,14 +1,18 @@
 # Makefile
 
-all: boot.bin
+all: disk.img
 
 boot.bin: boot/boot.asm
 	nasm -I include/ -f bin $< -o $@
 
-run: boot.bin
-	qemu-system-i386 -drive format=raw,file=$<
+disk.img: boot.bin
+	dd if=/dev/zero of=disk.img bs=512 count=20480 2>/dev/null  # 创建10MB硬盘镜像
+	dd if=boot.bin of=disk.img conv=notrunc 2>/dev/null
+# 软盘启动
+run: disk.img
+	qemu-system-i386 -drive file=disk.img,format=raw,if=floppy
 
 clean:
-	rm -f *.bin
+	rm -f boot.bin disk.img
 
 .PHONY: all run clean
--- boot/boot.asm
+++ boot/boot.asm
@@ -1,25 +1,59 @@
 ; boot.asm BIOS加载MBR
 
 [BITS 16]        ; 生成16位代码（实模式）
-[ORG 0x7C00]     ; 被加载到内存地址 0x7C00
-%include "bios_calls.inc"
+[ORG 0x7C00]     ; 被加载到内存地址 0x7C00 (这个是当前的基址)
 
+%include "bios_calls.inc"
+; 常量
+End2B equ 0xAA55
+
+start:
     ; 在实模式下，内存地址=段寄存器(ds)×16+偏移地址(si)
     ; 注意代码里计算的都是相对地址，最终的物理地址还要加上ORG地址
+    xor ax, ax
     mov ax, cs
     mov ds, ax
-
-    mov al, menu_msg ; 报错
+    
+    ; 显示启动消息
+    mov si, Booting_msg
     call print_string
+    
+    ; 尝试读取MBR（第一个扇区的内容）
+    mov ax, 0x07E0; Ax = Ah + Al
+    mov es, ax    ; ES:BX 是磁盘缓冲区的标准
+    xor bx, bx    ; 偏移量
+    
+    mov ah, 0x02 ; 读取扇区
+    mov al, 1    ; 读取1个扇区
+    mov ch, 0x00 ; 柱面0
+    mov cl, 0x01 ; 扇区1
+    mov dh, 0x00 ; 磁头0
+    mov dl, 0x00 ; 软盘启动
 
-    mov ah, KEY_INPUT_WAIT
-    int 0x16 ; 等待键盘输入
+    int 0x13
+    jc disk_error
+
+    mov si, success_msg
+    call print_string
 
     jmp $ ; 死循环
 
+disk_error:
+    mov si, disk_error_msg
+    call print_string
+
+    ; 显示错误代码（AH中的值）
+    mov al, ah
+    call print_hex_byte ; 打印十六进制数
+
+    mov ah, KEY_INPUT_WAIT
+    int 0x16
+
+    ret
+
 ; 打印字符串
 print_string:
-    mov ah, 0x0E ; BIOS 功能号：在屏幕上显示字符
+    mov ah, VIDEO_TELETYPE ; BIOS 功能号：在屏幕上显示字符
 .repeat:
     lodsb           ; mov al, [ds:si] inc si
     cmp al, 0       ; 检查是否是字符串结尾（0）
@@ -29,18 +63,59 @@ print_string:
 .done:
     ret
 
+; 换行函数
+print_newline:
+    mov ah, VIDEO_TELETYPE
+    mov al, 13        ; 回车CR
+    int 0x10
+    mov al, 10        ; 换行LF  
+    int 0x10
+    ret
+
+; 打印十六进制字节
+print_hex_byte:
+    push ax
+    push bx
+    
+    mov bl, al ; 备份al的值
+    shr al, 4  ; 取出高4位
+    call .print_digit
+    mov al, bl ; 恢复原值
+    and al, 0x0F ; 取出低4位
+    call .print_digit
+    call print_newline
+
+    pop bx
+    pop ax
+    ret
+.print_digit:
+    cmp al, 10
+    jb .digit
+    add al, 'A' - 10
+    jmp .print
+.digit:
+    add al, '0'
+.print:
+    mov ah, VIDEO_TELETYPE
+    int 0x10
+    ret
+
+
 ; 调试暂停
 debug_pause:
     mov si, debug_msg
     call print_string
-    mov ah, 0x00
-    int 0x16
+    mov ah, KEY_INPUT_WAIT
+    int 0x16                ; 暂停，等待键盘输入
+    mov si, end_debug_msg
+    call print_string
     ret
 
-press_key_msg db 'Press any key to boot...', 13, 10, 0
-hello_msg db 'Hello World! Booting...', 13, 10, 0
-menu_msg db 'Start Menu, please choose which one...', 13, 10, 0
+success_msg db "Sucessful disk read", 13, 10, 0
+disk_error_msg db "Error disk read", 13, 10, 0 
+Booting_msg db 'Sucessful Booting...', 13, 10, 0
 debug_msg db 'Start Debug, debug info...', 13, 10, 0
+end_debug_msg db 'End Debug, debug end...', 13, 10, 0
 
 times 510 - ($ - $$) db 0 ; (510 - 已使用字节量)
-dw 0xAA55 ; 必须写入0xAA55这个是BIOS判断标志
+dw End2B ; 必须写入0xAA55这个是BIOS判断标志
