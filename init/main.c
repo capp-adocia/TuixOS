13dca65e605595fa069771b4a1bd7f01ec008732
capp-adocia <2636427505@qq.com>
2025-11-07 09:42:04 +0800

    修复好grub导致的中断重启，原因是没有覆盖gdt表

--- init/main.c
+++ init/main.c
@@ -57,11 +57,70 @@ void init_keyboard_system(void)
     
     kprint(11, 0, "Keyboard system ready. Start typing...");
 }
+struct gdt_entry {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_middle;
+    uint8_t access;
+    uint8_t granularity;
+    uint8_t base_high;
+} __attribute__((packed));
 
-void kernel_main(unsigned int magic, unsigned int addr)
+struct gdt_ptr {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+struct gdt_entry gdt_entries[3];
+void setup_gdt() {
+    // 空描述符 (0x00)
+    gdt_entries[0].limit_low = 0;
+    gdt_entries[0].base_low = 0;
+    gdt_entries[0].base_middle = 0;
+    gdt_entries[0].access = 0;
+    gdt_entries[0].granularity = 0;
+    gdt_entries[0].base_high = 0;
+    
+    // 代码段 (0x08)
+    gdt_entries[1].limit_low = 0xFFFF;
+    gdt_entries[1].base_low = 0;
+    gdt_entries[1].base_middle = 0;
+    gdt_entries[1].access = 0x9A;        // 存在，环0，代码段，可读
+    gdt_entries[1].granularity = 0xCF;   // 4K粒度，32位模式
+    gdt_entries[1].base_high = 0;
+    
+    // 数据段 (0x10)
+    gdt_entries[2].limit_low = 0xFFFF;
+    gdt_entries[2].base_low = 0;
+    gdt_entries[2].base_middle = 0;
+    gdt_entries[2].access = 0x92;        // 存在，环0，数据段，可写
+    gdt_entries[2].granularity = 0xCF;   // 4K粒度，32位模式
+    gdt_entries[2].base_high = 0;
+}
+void kernel_main(uint32_t magic, uint32_t mbi_addr)
 {
     // 基础显示
     init();
+    setup_gdt();
+    struct gdt_ptr old_gdt;
+    asm volatile ("sgdt %0" : "=m"(old_gdt));
+    kprintf(16, 0, "GRUB GDT: base=0x%x, limit=0x%x", old_gdt.base, old_gdt.limit);
+    struct gdt_ptr new_gdt_ptr;
+    new_gdt_ptr.limit = sizeof(gdt_entries) - 1;
+    new_gdt_ptr.base = (uint32_t)&gdt_entries;
+
+    asm volatile("lgdt %0" : : "m"(new_gdt_ptr));
+
+    asm volatile(
+        "ljmp $0x08, $1f\n"
+        "1:\n"
+        "mov $0x10, %ax\n"
+        "mov %ax, %ds\n"
+        "mov %ax, %es\n"
+        "mov %ax, %fs\n"
+        "mov %ax, %gs\n"
+        "mov %ax, %ss\n"
+    );
+    
     // 内存初始化
     init_physical_memory();
     // 初始化堆,必须在内核初始化后做
@@ -77,6 +136,6 @@ void kernel_main(unsigned int magic, unsigned int addr)
     kprint(12, 0, "System ready. Keyboard should work now.");
 
     while(1){
-        asm volatile("hlt");
+        // asm volatile("hlt");
     }
 }
--- kernel/isr.c
+++ kernel/isr.c
@@ -7,7 +7,7 @@ void isr_##num(void) { \
     __asm__ volatile("pushl $0\n\t" "pushl $" #num "\n\t" "jmp isr_common"); \
 }
 
-#define X(num) ISR_NOERRCODE(num);
+#define X(num) ISR_NOERRCODE(num)
     IDT_LIST_EXP // 0-31: CPU 异常
     IDT_LIST_PIC // 32-47: 硬件中断 (IRQ0-IRQ15)
     IDT_LIST_OTHER // 48-255: 保留和用户定义中断
